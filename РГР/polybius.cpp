fe98e2b28c2ea19d7963ffef56bfc809a629f282
LinkerEduard <123632197+LinkerEduard@users.noreply.github.com>
2023-06-07 01:01:27 +0700

    Update polybius.cpp

--- РГР/polybius.cpp
+++ РГР/polybius.cpp
@@ -1,6 +1,6 @@
 #include "polybius.h"
 
-const char TABLE[12][11] = { {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'},
+const char TABLE[99][99] = { {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'},
                            {'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'},
                            {'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g'},
                            {'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'},
@@ -15,8 +15,8 @@ const char TABLE[12][11] = { {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
 
 void findPosition(char letter, int& row, int& col)
 {
-    for (int i = 0; i < 12; ++i) {
-        for (int j = 0; j < 11; ++j) {
+    for (int i = 0; i < 99; ++i) {
+        for (int j = 0; j < 99; ++j) {
             if (TABLE[i][j] == letter) {
                 row = i;
                 col = j;
@@ -64,7 +64,7 @@ void polybius_decrypt(string inputFileFileName, string outputFileFileName)
     string line;
     while (getline(inputFile, line)) {
         for (size_t i = 0; i < line.length(); ++i) {
-            if ((line[i]-'0'>=0) && (line[i] - '0' <= 9)) {
+            if ((line[i]-'0'>=0) && (line[i] - '0' <= 9) && (line[i+1]!=0) && (line[i + 2] != 0) && (line[i + 3] != 0) && (line[i+1] - '0' >= 0) && (line[i+1] - '0' <= 9) &&(line[i + 2] - '0' >= 0) && (line[i + 2] - '0' <= 9) && (line[i + 3] - '0' >= 0) && (line[i + 3] - '0' <= 9)) {
                 int row = (line[i]-'0') * 10 + (line[i + 1]-'0');
                 int col = (line[i + 2] - '0') * 10 + (line[i + 3] - '0');
                 outputFile << TABLE[row][col];
